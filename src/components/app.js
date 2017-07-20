20cc6c665e2e1951e89536e72ae3e4c38d1a0b3e
Elena Hickey <subhickeyelena@gmail.com>
2017-07-20 15:56:07 -0400

    bable from devdep to dep

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import NavBarHeader from "./nav";
+import NavBarHeader from "./Nav";
 import Video from "./video/video";
 import Signin from "./auth/signin";
 import NewItem from "./list/newitem";
@@ -8,6 +8,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
+
 				<NavBarHeader />
 				<Video />
 				{this.props.children}
